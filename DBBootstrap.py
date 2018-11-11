14e9412cdbba7d18694e3cbe87701a7add891c67
arun <gimblett44@gmail.com>
2018-11-11 20:35:21 +0000

    removed unused db create statement since its handled by docker on init

--- DBBootstrap.py
+++ DBBootstrap.py
@@ -26,22 +26,6 @@ def verify_database_exists():
 
 
 if __name__ == "__main__":
-    print("Checking to see if %s database table exists" % (database))
-
-    if verify_database_exists():
-        print("Database %s exists!" % (database))
-    else:
-        print("Was unable to find %s on host %s" % (database, host))
-        result = input("Would you like to create %s on host%s:%s ? [y/n]" % (database, host, port))
-        if result.lower() == 'y':
-            conn = (create_engine(
-                "postgresql://%s:%s@%s:%s/%s" % (username, password, host, port, 'postgres')
-            )).connect()
-            # Have to commit to end the transaction
-            conn.execute('COMMIT')
-            conn.execute("CREATE DATABASE %s" % database)
-            conn.close()
-            print("Database %s created!" % database)
 
     metadata = MetaData(get_engine())
     table = Table('example', metadata,
