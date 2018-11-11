28887bd9e6f962ed89341a1ae3d0c5b71da22ce3
arun <gimblett44@gmail.com>
2018-11-11 17:58:08 +0000

    Completed post insert for api model endpoint

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+from sqlalchemy import *
+from ModelRepository import ModelRepository
+from model import Model
 
 app = Flask(__name__)
 
+username = 'postgres'
+password = 'password'
+host = 'postgres'
+port = '5432'
+database = 'test'
 
-@app.route('/test', methods=['GET', 'POST'])
-def parse_response():
+repository = ModelRepository(
+            create_engine("postgresql://%s:%s@%s:%s/%s" % (username, password, host, port, database))
+        )
+
+@app.route('/', methods=['POST'])
+def insert_model():
     if request.method == 'POST':
         print(request.form)
-        return 'received post'
-    return 'woo2'
 
+        repository.put(
+            Model(request.form['text_value'], request.form['select_value'])
+        )
+        return Response(status=200)
+
+@app.route('/list', methods=['GET'])
+def get_all_models():
+    print(repository.get_all())
+    return Response(status=200)
 
 if __name__ == '__main__':
     app.run()
