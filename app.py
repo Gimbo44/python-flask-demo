92d5eb9a781fa1b87f661ea5b2f5013cf650224c
arun <gimblett44@gmail.com>
2018-11-11 21:25:03 +0000

    remove getList api endpoint

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
+import os
+
 from flask import Flask, request, Response
 from sqlalchemy import *
 from ModelRepository import ModelRepository
+from dotenv import load_dotenv
+from pathlib import Path
 from model import Model
 
 app = Flask(__name__)
 
-username = 'postgres'
-password = 'password'
-host = 'postgres'
-port = '5432'
-database = 'test'
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+username = os.getenv('POSTGRES_USERNAME')
+password = os.getenv('POSTGRES_PASSWORD')
+host = os.getenv('POSTGRES_HOST')
+port = os.getenv('POSTGRES_PORT')
+database = os.getenv('POSTGRES_DATABASE')
 
 repository = ModelRepository(
             create_engine("postgresql://%s:%s@%s:%s/%s" % (username, password, host, port, database))
@@ -18,17 +25,12 @@ repository = ModelRepository(
 @app.route('/model', methods=['POST'])
 def insert_model():
     if request.method == 'POST':
-        print(request.form)
 
         repository.put(
             Model(request.form['text_value'], request.form['select_value'])
         )
         return Response(status=200)
 
-@app.route('/model/list', methods=['GET'])
-def get_all_models():
-    print(repository.get_all())
-    return Response(status=200)
 
 if __name__ == '__main__':
     app.run()
